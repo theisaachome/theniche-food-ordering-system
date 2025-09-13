3be0ab105937a6fd92efff814b893bc93ebd3f6b
theisaachome <65864160+theisaachome@users.noreply.github.com>
2025-09-13 18:26:32 +0800

    init project

---
+++ src/main/java/com/theniche/order/mgmt/App.java
@@ -0,0 +1,13 @@
+package com.theniche.order.mgmt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
