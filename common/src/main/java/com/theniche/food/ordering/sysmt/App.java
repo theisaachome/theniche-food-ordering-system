3d0be598c53136c218e9737e05de29f86d522b0d
theisaachome <65864160+theisaachome@users.noreply.github.com>
2025-09-13 19:43:34 +0800

    order-service modules structure.

---
+++ common/src/main/java/com/theniche/food/ordering/sysmt/App.java
@@ -0,0 +1,13 @@
+package com.theniche.food.ordering.sysmt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
